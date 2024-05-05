e3281241fa62dcae046b0208badfcba42af9b789
vijaykumarme <vijaykdev19@gmail.com>
2024-05-05 12:51:52 +0530

    Commited after successfully connected with postgresql from vm

--- server/db.js
+++ server/db.js
@@ -1,34 +1,13 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password: "Kumar@369*",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
-});
+    user: 'postgres',       //user
+    host: '', //host ip address
+    database: '',   //database
+    password: '', // Password
+    port: 5432, // Default PostgreSQL port
+  });
 
 module.exports = pool;
 
-// const {Client} = require("pg");
-
-// const client = new Client({
-//     user: "postgres",
-//     password: "Kumar@369*",
-//     host: "localhost",
-//     port: 5432,
-//     database: "perntodo"
-// });
-
-// client.connect();
-
-// client.query(`Select * from todo`,(err,res) => {
-//     if(!err) {
-//         console.log(res.rows);
-//     } else {
-//         console.log(err.message);
-//     }
-// })
-
-// client.end
 
