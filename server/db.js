a4d4e635c9484e6fa08a8b9b5a3747c644f4ee4a
vijaykumarme <vijaykdev19@gmail.com>
2024-05-05 14:39:21 +0530

    After adding env variables

--- server/db.js
+++ server/db.js
@@ -1,13 +1,18 @@
-const Pool = require("pg").Pool
+const {Pool} = require("pg");
 
-const pool = new Pool({
-    user: 'postgres',       //user
-    host: '', //host ip address
-    database: '',   //database
-    password: '', // Password
-    port: 5432, // Default PostgreSQL port
-  });
+const pool = new Pool();
+module.exports = {
+  query: (text,params) => pool.query(text,params)
+}
 
-module.exports = pool;
+// const pool = new Pool({
+//     user: 'postgres',       //user
+//     host: '', //host ip address
+//     database: '',   //database
+//     password: '', // Password
+//     port: 5432, // Default PostgreSQL port
+//   });
+
+// module.exports = pool;
 
 
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -71,7 +72,8 @@ app.delete("/todos/:id",async (req,res) => {
     }
 })
 
+const port = process.env.PORT || 5000
 
-app.listen(5000,() => {
-    console.log("server has started on port 5000");
+app.listen(port,() => {
+    console.log(`server has started on port ${port}`);
 });
